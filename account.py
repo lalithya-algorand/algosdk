cdbc1380eb96e93093cbf1794eaceffa9e710b7e
lalithyavarmaa <lalithyavarmasamanthapudi427@gmail.com>
2025-01-07 22:19:07 +0530

    Added algorand account creation

---
+++ account.py
@@ -0,0 +1,20 @@
+from algosdk import account, encoding, mnemonic
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+private_key, address = account.generate_account()
+print("Private key:", private_key)
+print("Address:", address)
+
+mnemon = mnemonic.from_private_key(private_key)
+print("Menomonic:",mnemon)
+
+derived_private_key = mnemonic.to_private_key(mnemon)
+print("Private key derived from mnemonic:", derived_private_key)
+
+print("Comparision between two private keys:", private_key == derived_private_key)
+
+env_private = os.getenv("PRIVATE_KEY")
+print("Private key from .env:", env_private)
+print("Address from .env", account.address_from_private_key(env_private))
